e600f08c2733a2a3d247169039f86c4da72e9649
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-10-07 16:11:46 +0200

    Add articles/:id route

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ app.get("/articles", (req, res) => {
   res.status(200).json(articles);
 });
 
+app.get("/articles/:id", (req, res) => {
+  let article = articles.filter(el => el.id == req.params.id);
+
+  res.header('Access-Control-Allow-Origin', '*');
+  if (Object.keys(article).length === 0) {
+    res.status(404).send();
+  } else {
+    res.status(200).json(article);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
