b91b9982ef2c88c28e03ca26c61d616f576dfb28
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-10-22 02:40:23 +0200

    Add basic session handling

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const secrets = require("./secrets.js");
 const express = require("express");
 const bcrypt = require("bcrypt");
 const mariadb = require("mariadb");
-const jwt = require("jsonwebtoken");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
 const upload = multer({ dest: secrets.STATIC_FOLDER });
 const app = express();
 const port = 3000;
+const env = app.get('env');
 
 const pool = mariadb.createPool({
      host: secrets.SQL_HOST, 
@@ -40,27 +41,17 @@ async function dbQueryUser(user) {
   return dbQuery(`SELECT * FROM users WHERE username=('${user}')`);
 }
 
-function tokenVerify(req, res, next) {
-  let token = req.headers.authorization.split(' ')[1];
-  let payload = jwt.verify(token, secrets.SHARED_SECRET);
-
-  if (token === 'null') {
-    return res.status(401).send();
+app.use(session({
+  secret: secrets.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  unset: 'keep',
+  name: 'sid',
+  store: env === 'production' ? sessionStore : new session.MemoryStore,
+  cookie: {
+    secure: env === 'production' ? true : false,
   }
-  if (!payload) {
-    return res.status(401).send();
-  }
-  req.payload = payload;
-  next();
-}
-
-function tokenAdminVerify(req, res, next) {
-  if (req.payload.role !== 0) {
-    return res.status(401).send();
-  }
-  next();
-}
-
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -84,7 +75,7 @@ app.get("/categories", async (req, res) => {
   res.status(200).json(ret);
 });
 
-app.post("/articles", [tokenVerify, tokenAdminVerify], async (req, res) => {
+app.post("/articles", async (req, res) => {
   const ret = await dbQuery(`INSERT INTO articles (name, subtitle, description, price, category_id)
   VALUES ('${req.body.name}', '${req.body.subtitle}', '${req.body.description}', ${req.body.price}, ${req.body.category_id})`);
   
@@ -106,7 +97,7 @@ app.get("/articles/:id", async (req, res) => {
   }
 });
 
-app.put("/articles/:id", [tokenVerify, tokenAdminVerify], async (req, res) => {
+app.put("/articles/:id", async (req, res) => {
   const ret = await dbQuery(`UPDATE articles 
   SET name='${req.body.name}', subtitle='${req.body.subtitle}', description='${req.body.description}', price=${req.body.price}, category_id=${req.body.category_id} WHERE id=(${req.params.id})`);
 
@@ -118,7 +109,7 @@ app.put("/articles/:id", [tokenVerify, tokenAdminVerify], async (req, res) => {
   }
 });
 
-app.delete("/articles/:id", [tokenVerify, tokenAdminVerify], async (req, res) => {
+app.delete("/articles/:id", async (req, res) => {
   const ret = await dbQuery(`DELETE FROM articles WHERE id=(${req.params.id})`);
 
   if (ret.affectedRows === 0) {
@@ -128,7 +119,7 @@ app.delete("/articles/:id", [tokenVerify, tokenAdminVerify], async (req, res) =>
   }
 });
 
-app.post("/img/articles/:id", [tokenVerify, tokenAdminVerify, upload.single("article")], async (req, res) => {
+app.post("/img/articles/:id", upload.single("article"), async (req, res) => {
   if (!req.file) {
     res.status(204).send();
   } else {
@@ -142,7 +133,7 @@ app.post("/img/articles/:id", [tokenVerify, tokenAdminVerify, upload.single("art
   }
 });
 
-app.delete("/img/articles/:id", [tokenVerify, tokenAdminVerify], async (req, res) => {
+app.delete("/img/articles/:id", async (req, res) => {
   const ret = await dbQuery(`DELETE FROM images WHERE id=(${req.params.id})`);
 
   if (ret.affectedRows === 0) {
@@ -180,16 +171,13 @@ app.post("/auth/signin", async (req, res) => {
     const match = await bcrypt.compare(req.body.password, usr[0].password);
   
     if (match) {
-      let token = jwt.sign({ id: usr[0].id, role: usr[0].role }, secrets.SHARED_SECRET);
-
-      res.header('Authorization', 'Bearer ' + token)
-      res.header('Access-Control-Expose-Headers', 'Authorization');
-      res.status(200).send();
+      req.session.authenticated = true;
+      res.status(200).send('OK');
     } else {
-      res.status(401).send("Invalid password");
+      throw new Error('Invalid credentials');
     }
   } catch (err) {
-    res.status(401).send("Invalid user");
+    res.status(401).send(err.message);
   }
 });
 
