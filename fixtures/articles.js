8db9189a2700cd86d135b055f5284466362c7235
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-08 08:59:34 +0100

    Add fixtures

--- fixtures/articles.js
+++ fixtures/articles.js
@@ -10,4 +10,6 @@ Article.bulkCreate([
   {sku: 'BRI-00001', name: 'Perceuse', subtitle: 'Metabo', description: 'Très pratique', price: 99.99, CategoryId: 4},
   {sku: 'LIB-00001', name: 'Les Fleurs du mal', subtitle: 'Charles Baudelaire', description: 'Un grand classique', price: 9.99, CategoryId: 2},
   {sku: 'LIB-00002', name: 'L\'Art de la Guerre', subtitle: 'Sun Tzu', description: 'Un grand classique', price: 9.99, CategoryId: 2},
+  {sku: 'BRO-00001', name: 'Armoire Napoléon III', subtitle: 'Armoire d\'Apparat Napoléon III', description: 'Exceptionnelle armoire d\'apparat de style et d\'époque Napoléon III à façade galbée ouvrant deux vantaux présentant des médaillons en marquèterie florale sur fond d\'ébène. Partie supérieure deforme doucine marquétée, beau piètement galbé à décor de fleurettes.\nIntérieur en placage de palissandre de rio sur un bâti en chêne.\nTravail parisien de très belle qualité.\nTrès bon état - entièrement restauré par notre atelier - finition vernis au tampon', price: 5000.00, CategoryId: 5},
+  {sku: 'LOI-00001', name: 'Jeu d\'échecs', subtitle: 'Jeu en bois', description: 'Jeu d\'échecs pliable Pour l\'emmener partout, en voiture, en voyage, à la mer ou la montagne.\n\nEn bois, fabriqué en Allemagne', price: 19.99, CategoryId: 3},
 ]);
--- fixtures/images.js
+++ fixtures/images.js
@@ -17,4 +17,9 @@ Image.bulkCreate([
   {filename: 'occ00002_3.png', mimetype: 'image/png', path: 'static\\occ00002_3.png', ArticleId: 4},
   {filename: 'lib00001_1.jpg', mimetype: 'image/jpeg', path: 'static\\lib00001_1.jpg', ArticleId: 6},
   {filename: 'lib00002_1.jpg', mimetype: 'image/jpeg', path: 'static\\lib00002_1.jpg', ArticleId: 7},
+  {filename: 'bro00001_1.jpg', mimetype: 'image/jpeg', path: 'static\\bro00001_1.jpg', ArticleId: 8},
+  {filename: 'bro00001_2.jpg', mimetype: 'image/jpeg', path: 'static\\bro00001_2.jpg', ArticleId: 8},
+  {filename: 'bro00001_3.jpg', mimetype: 'image/jpeg', path: 'static\\bro00001_3.jpg', ArticleId: 8},
+  {filename: 'loi00001_1.jpg', mimetype: 'image/jpeg', path: 'static\\loi00001_1.jpg', ArticleId: 9},
+  {filename: 'loi00001_2.jpg', mimetype: 'image/jpeg', path: 'static\\loi00001_2.jpg', ArticleId: 9},
 ]);
